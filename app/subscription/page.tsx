7f91e29eda0d90c746c2e104f5788fb18e94d444
Vinicius Rorato da Cruz <vinicius.cruz@novaagri.com.br>
2024-11-13 08:04:53 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+  return <h1>subscription</h1>;
+};
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return <h1>transaction pagge</h1>;
+};
+
+export default Transactions;
